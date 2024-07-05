8607bd9e53d6bb11915c55dd0068cb2579e720c5
Arsene ndimanya <146384225+Arsenendimanya151311@users.noreply.github.com>
2024-07-05 08:52:21 +0300

    Our main catalog page

---
+++ Templates/KivuCatalog.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style type="text/css">
+      *{
+         text-decoration: none; }   
+         .navbar{
+          background:crimson; font-family: calibri;  padding-right: 15px;padding-left: 15px;
+         }
+         .navdiv{
+             display: flex;align-items: center;justify-content: space-between;
+ 
+         }
+         .logo a{
+             font-size: 35px;font-weight: 600;color: white;
+         }
+         li{
+             list-style: none; display: inline-block;
+         }
+         li a{
+             color: white; font-size: 18px; font-weight: bold;margin-right: 25px;
+         }
+         button{
+             background-color: black;margin-left: 10px;border-radius: 10px; padding: 10px;width: 90px;
+         }
+         button{
+             color: white;font-weight: bold;font-size: 15px;
+         }
+     </style>
+ </head>
+ <body>
+</head>
+<body>
+  <nav class="navbar"> 
+    <div class="navdiv">
+        <div class="logo"><a href="#"></a><h2>KIVU SNEAKERS</h2></div>
+        <ul>
+            <li> <a href="About.html"> About Us </a> </li>
+            <li>  <a href="index.html"> Home </a>  </li>
+            <li> <a href="KivuCatalog.html">KivuCatalog</a> </li>
+            <li>  <a href="KidsCatolog.html">kidsCatalog</a></li>
+            <li> <a href="WomanKicksCatalog.html">WomanKicksCatalog</a> </li>
+            <li> <a href="Apparel.html">Apparel</a> </li>
+            <li>  <a href="Contact.html">Contact</a> </li>
+            <button><a href="#">Login</a></button>
+            <button><a href="#">SignUp</a></button>
+        </ul>
+    
+    </div>
+    
+    </nav>
+
+   
+
+
+
+    <p>
+   
+<br>
+<style>
+
+img{
+    width: 200px;
+    height:300px ;
+    object-fit: cover;
+    margin: 1px;
+ }
+
+    * {
+  box-sizing: border-box;
+}
+
+.img-container {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+}
+
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+</style>
+
+
+
+
+        
+<div class="clearfix">
+    <div class="img-container">
+    <img src="images/JORDAN  KIDS PRS.jpg" alt="sneakers" style="width:100%">
+    <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+    <p><h3>JORDAN</h3></p>
+    <p style="color:red;">150$</p>
+
+
+    </div>
+    <div class="img-container">
+        <img src="images/NIKE ZOOM.jpg" alt="Mountains" style="width:100%">
+        <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+        <p><h3> NIKE ZOOM</h3></p>
+        <p style="color:red;">180$</p>
+   </div>
+    <div class="img-container">
+    <img src="images/KIDS ADIDAS BST.jpg" alt="Forest" style="width:100%">
+    <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+    <p><h3>ADIDAS </h3></p>
+    <p style="color:red;">100$</p>
+ </div>
+  
+    
+    <div class="img-container">
+        <img src="images/AIR MAX.jpg" alt="Mountains" style="width:100%">
+        <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+        <p><h3>AIR MAX</h3></p>
+        <p style="color:red;">220$</p>
+    
+        </div>
+        <div class="img-container">
+            <img src="images/JJORDAN 1 LOW.jpg" alt="Mountains" style="width:100%">
+            <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+            <p><h3> JORDAN 1 LOW</h3></p>
+            <p style="color:red;">750$</p>
+        
+            </div>
+            <div class="img-container">
+                <img src="images/JORDAN 1 LOW CHOCOLAT.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> JORDAN CHOCOLATE </h3></p>
+                <p style="color:red;">280$</p>
+           </div>
+           <div class="img-container">
+            <img src="images/JORDAN 1 LA.jpg" alt="Mountains" style="width:100%">
+            <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+            <p><h3> JORDAN L.A </h3></p>
+            <p style="color:red;">180$</p>
+       </div>
+       <div class="img-container">
+        <img src="images/KIDS AIRMAX.jpg" alt="Mountains" style="width:100%">
+        <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+        <p><h3> NEW AIR MAX</h3></p>
+        <p style="color:red;">750$</p>
+    
+        </div>
+        <div class="img-container">
+            <img src="images/Old adidas.jpg" alt="Mountains" style="width:100%">
+            <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+            <p><h3> OLD</h3></p>
+            <p style="color:red;">550$</p>
+        
+            </div>
+</div>
+
+
+<h1 style ="text-align: center ;
+                                                     color:rgb(0, 0, 0); 
+                                                     text-decoration: underline; 
+                                                     text-transform: uppercase;
+                                                      font-style: italic; 
+                                                      font-weight: 600;"> </h1>
+                                                   
+
+                                                    <form action="" methods="post">
+                                                    <label for="fn"> Size</label><br>
+                                                    <input type="text" id="fn">
+<br>
+                                                   
+                                                  
+
+               
+    
+                
+
+                        <ol >
+                            <input type="checkbox"  Pilot=" Pilot " value="on" /> Delivery
+                            <input type="checkbox"  Pilot=" Pilot " value="on" /> Own pick up
+                          
+                            
+                        </ol><form action=""></form>
+                    <form>
+
+                    <button>Submit</button>
+                    <hr size="8" width="100%" color="red"> 
+                    <p><center>Copyright KIVU Kicks 2024 &copy; Allrights reserved</center> </p>
+
+                    </form>
+                    <style> body {
+                        background-image: url(images/bk.jpg);
+                    }
+                            </style>
+</body>
+</html>
