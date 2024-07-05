885d97874faeb7e87d84cea67ed94c2a3f625ce7
Arsene ndimanya <146384225+Arsenendimanya151311@users.noreply.github.com>
2024-07-05 08:55:24 +0300

    Our  firtst women's catalog

---
+++ Templates/WomanKicksCatalog.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style type="text/css">
+      *{
+         text-decoration: none; }   
+         .navbar{
+          background:crimson; font-family: calibri;  padding-right: 15px;padding-left: 15px;
+         }
+         .navdiv{
+             display: flex;align-items: center;justify-content: space-between;
+ 
+         }
+         .logo a{
+             font-size: 35px;font-weight: 600;color: white;
+         }
+         li{
+             list-style: none; display: inline-block;
+         }
+         li a{
+             color: white; font-size: 18px; font-weight: bold;margin-right: 25px;
+         }
+         button{
+             background-color: black;margin-left: 10px;border-radius: 10px; padding: 10px;width: 90px;
+         }
+         button{
+             color: white;font-weight: bold;font-size: 15px;
+         }
+     </style>
+ </head>
+ <body>
+</head>
+<body>
+  <nav class="navbar"> 
+    <div class="navdiv">
+        <div class="logo"><a href="#"></a><h2>KIVU SNEAKERS</h2></div>
+        <ul>
+            <li> <a href="About.html"> About Us </a> </li>
+            <li>  <a href="index.html"> Home </a>  </li>
+            <li> <a href="KivuCatalog.html">KivuCatalog</a> </li>
+            <li>  <a href="KidsCatolog.html">kidsCatalog</a></li>
+            <li> <a href="WomanKicksCatalog.html">WomanKicksCatalog</a> </li>
+            <li> <a href="Apparel.html">Apparel</a> </li>
+            <li>  <a href="Contact.html">Contact</a> </li>
+            <button><a href="#">Login</a></button>
+            <button><a href="#">SignUp</a></button>
+        </ul>
+    
+    </div>
+    
+    </nav>
+
+   
+
+
+
+    <p>
+   
+<br>
+<style>
+
+img{
+    width: 200px;
+    height:300px ;
+    object-fit: cover;
+    margin: 1px;
+ }
+
+    * {
+  box-sizing: border-box;
+}
+
+.img-container {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+}
+
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+</style>
+
+
+
+
+        
+<div class="clearfix">
+    <div class="img-container">
+    <img src="images/ladie 1.jpg" alt="sneakers" style="width:100%">
+    <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+    <p><h3>Zara Green Boots</h3></p>
+    <p style="color:red;">1500$</p>
+
+
+    </div>
+    <div class="img-container">
+    <img src="images/ladie 10.jpg" alt="Forest" style="width:100%">
+    <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+    <p><h3>Zara Black Boots</h3></p>
+    <p style="color:red;">1000$</p>
+
+    </div>
+    <div class="img-container">
+    <img src="images/ladie 11.jpg" alt="Mountains" style="width:100%">
+    <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+    <p><h3>New Generation Louboutin Red Cross Escarpin</h3></p>
+    <p style="color:red;">700$</p>
+    
+</div>
+    
+    <div class="img-container">
+        <img src="images/ladie 2.jpg" alt="Mountains" style="width:100%">
+        <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+        <p><h3> OFF White Black Boots</h3></p>
+        <p style="color:red;">200$</p>
+    
+        </div>
+        <div class="img-container">
+            <img src="images/ladie 3.jpg" alt="Mountains" style="width:100%">
+            <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+            <p><h3>New  Black Cross Escarpin</h3></p>
+            <p style="color:red;">700$</p>
+        
+            </div>
+            <div class="img-container">
+                <img src="images/ladie 4.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> New Louboutin Red Cross Escarpin</h3></p>
+                <p style="color:red;">800$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/ladie 5.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3>Louboutin Red Cross Escarpin</h3></p>
+                <p style="color:red;">700$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/ladie 6.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Balmain Escarpin</h3></p>
+                <p style="color:red;">800$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/ladie 7.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Zara Red Cross Escarpin</h3></p>
+                <p style="color:red;">650$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/ladie 9.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Red Cross Escarpin</h3></p>
+                <p style="color:red;">770$</p>
+            </div> 
+            
+            <div class="img-container">
+                <img src="images/LOUBOUTIN.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Louboutin </h3></p>
+                <p style="color:red;">1000$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/ladie 11.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Escarpin Rouge</h3></p>
+                <p style="color:red;">770$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/ladie 12.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> New balance</h3></p>
+                <p style="color:red;">270$</p>
+            </div> 
+            <div class="img-container">
+                <img src="images/ladie 13.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Gym Shoes</h3></p>
+                <p style="color:red;">70$</p>
+            </div> 
+           
+            
+                <div class="img-container">
+                    <img src="images/ladie 18.jpg" alt="Mountains" style="width:100%">
+                    <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                    <p><h3> Puma Sneakers </h3></p>
+                    <p style="color:red;">170$</p>
+                </div>  
+  </div>
+
+
+
+
+<h1 style ="text-align: center ;
+                                                     color:rgb(0, 0, 0); 
+                                                     text-decoration: underline; 
+                                                     text-transform: uppercase;
+                                                      font-style: italic; 
+                                                      font-weight: 600;"> </h1>
+                                                   
+
+                                                    <form action="" methods="post">
+                                                    <label for="fn"> Size</label><br>
+                                                    <input type="text" id="fn">
+<br>
+                                                   
+                                                  
+
+               
+    
+                
+
+                        <ol >
+                            <input type="checkbox"  Pilot=" Pilot " value="on" /> Delivery
+                            <input type="checkbox"  Pilot=" Pilot " value="on" /> Own pick up
+                          
+                            
+                        </ol><form action=""></form>
+                    <form>
+
+                    <button>Submit</button>
+                    <hr size="8" width="100%" color="red"> 
+                    <p><center>Copyright KIVU Kicks 2024 &copy; Allrights reserved</center> </p>
+
+                    </form>
+                    <style> body {
+                        background-image: url(images/bk.jpg);
+                    }
+                            </style>
+</body>
+</html>
