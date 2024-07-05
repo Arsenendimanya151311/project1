7ed6977c449de1ae41ba23de9e29a44c99f3221d
Arsene ndimanya <146384225+Arsenendimanya151311@users.noreply.github.com>
2024-07-05 08:54:28 +0300

    our Apparel catalog which contains Accessories such as t shirt,glasses etc

---
+++ Templates/Apparel.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style type="text/css">
+      *{
+         text-decoration: none; }   
+         .navbar{
+          background:crimson; font-family: calibri;  padding-right: 15px;padding-left: 15px;
+         }
+         .navdiv{
+             display: flex;align-items: center;justify-content: space-between;
+ 
+         }
+         .logo a{
+             font-size: 35px;font-weight: 600;color: white;
+         }
+         li{
+             list-style: none; display: inline-block;
+         }
+         li a{
+             color: white; font-size: 18px; font-weight: bold;margin-right: 25px;
+         }
+         button{
+             background-color: black;margin-left: 10px;border-radius: 10px; padding: 10px;width: 90px;
+         }
+         button{
+             color: white;font-weight: bold;font-size: 15px;
+         }
+     </style>
+ </head>
+ <body>
+</head>
+<body>
+  <nav class="navbar"> 
+    <div class="navdiv">
+        <div class="logo"><a href="#"></a><h2>KIVU SNEAKERS</h2></div>
+        <ul>
+            <li> <a href="About.html"> About Us </a> </li>
+            <li>  <a href="index.html"> Home </a>  </li>
+            <li> <a href="KivuCatalog.html">KivuCatalog</a> </li>
+            <li>  <a href="KidsCatolog.html">kidsCatalog</a></li>
+            <li> <a href="WomanKicksCatalog.html">WomanKicksCatalog</a> </li>
+            <li> <a href="Apparel.html">Apparel</a> </li>
+            <li>  <a href="Contact.html">Contact</a> </li>
+            <button><a href="#">Login</a></button>
+            <button><a href="#">SignUp</a></button>
+        </ul>
+    
+    </div>
+    
+    </nav>
+
+   
+
+
+
+    <p>
+   
+<br>
+<style>
+
+img{
+    width: 200px;
+    height:300px ;
+    object-fit: cover;
+    margin: 1px;
+ }
+
+    * {
+  box-sizing: border-box;
+}
+
+.img-container {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+}
+
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+</style>
+
+
+
+
+        
+<div class="clearfix">
+    <div class="img-container">
+    <img src="images/Apparel1.jpg" alt="sneakers" style="width:100%">
+    <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+    <p><h3>T-shirt Zone Gym</h3></p>
+    <p style="color:red;">15$</p>
+
+
+    </div>
+    <div class="img-container">
+    <img src="images/Apparel2.jpg" alt="Forest" style="width:100%">
+    <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+    <p><h3>T-shirt calvin</h3></p>
+    <p style="color:red;">10$</p>
+
+    </div>
+    <div class="img-container">
+    <img src="images/Apparel3.jpg" alt="Mountains" style="width:100%">
+    <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+    <p><h3>Couple shirt calvin</h3></p>
+    <p style="color:red;">70$</p>
+    
+</div>
+    
+    <div class="img-container">
+        <img src="images/Apparel9.jpg" alt="Mountains" style="width:100%">
+        <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+        <p><h3>T-shirt OFF White</h3></p>
+        <p style="color:red;">20$</p>
+    
+        </div>
+        <div class="img-container">
+            <img src="images/Apparel5.jpg" alt="Mountains" style="width:100%">
+            <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+            <p><h3> Black casual T-shirt</h3></p>
+            <p style="color:red;">70$</p>
+        
+            </div>
+            <div class="img-container">
+                <img src="images/Apparel6.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Adult Tie </h3></p>
+                <p style="color:red;">80$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/Apparel7.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> White</h3></p>
+                <p style="color:red;">70$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/Apparel8.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Blue Navy T-shirt</h3></p>
+                <p style="color:red;">80$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/Apparel10.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Luminary T-Shirt</h3></p>
+                <p style="color:red;">65$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/Apparel11.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Female Rayban</h3></p>
+                <p style="color:red;">70$</p>
+            </div> 
+            
+            <div class="img-container">
+                <img src="images/Apparel13.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Yellow rayban </h3></p>
+                <p style="color:red;">25$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/Apparel14.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> White shirt</h3></p>
+                <p style="color:red;">70$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/Apparel15.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Black rayban</h3></p>
+                <p style="color:red;">70$</p>
+                
+            </div>
+          
+            <div class="img-container">
+                <img src="images/Apparel17.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Couple Caps</h3></p>
+                <p style="color:red;">30$</p>
+            </div>
+            <div class="img-container">
+                <img src="images/Apparel18.jpg" alt="Mountains" style="width:100%">
+                <input type="checkbox"  Pilot=" Pilot " value="on" />Add to cart
+                <p><h3> Sun Glasses</h3></p>
+                <p style="color:red;">15$</p>
+            </div>
+            
+  </div>
+
+
+
+
+<h1 style ="text-align: center ;
+                                                     color:rgb(0, 0, 0); 
+                                                     text-decoration: underline; 
+                                                     text-transform: uppercase;
+                                                      font-style: italic; 
+                                                      font-weight: 600;"> </h1>
+                                                   
+
+                                                    <form action="" methods="post">
+                                                    <label for="fn"> Size</label><br>
+                                                    <input type="text" id="fn">
+<br>
+                                                   
+                                                  
+
+               
+    
+                
+
+                        <ol >
+                            <input type="checkbox"  Pilot=" Pilot " value="on" /> Delivery
+                            <input type="checkbox"  Pilot=" Pilot " value="on" /> Own pick up
+                          
+                            
+                        </ol><form action=""></form>
+                    <form>
+
+                    <button>Submit</button>
+                    <hr size="8" width="100%" color="red"> 
+                    <p><center>Copyright KIVU Kicks 2024 &copy; Allrights reserved</center> </p>
+
+                    </form>
+                    <style> body {
+                        background-image: url(images/bk.jpg);
+                    }
+                            </style>
+</body>
+</html>
