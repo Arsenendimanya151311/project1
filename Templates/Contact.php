8067a1cc328e937e97c9bee8b34f267d8d20d55a
Arsene ndimanya <146384225+Arsenendimanya151311@users.noreply.github.com>
2024-07-08 15:51:41 +0300

    gcfgjhhfuyfv

--- Templates/Contact.php
+++ Templates/Contact.php
@@ -133,10 +133,35 @@ echo "Connected successfully";
                 <li>+243 990000000 - Technical Assistance (No Sales)</li>
             </ul>
         </div>
+
+        <div class="row">
+    <div class="content">
+    <h1>Talk to Us</h1>
+    <form action="<?php print htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="contacts_form">
+        <label for="fn">Fullname:</label><br>
+        <input type="text" id="fn" placeholder="Fullname" name="fullname" required><br><br>
+
+        <label for="em">Email Address:</label><br>
+        <input type="email" id="em" placeholder="Email Address" name="email_address" required><br><br>
+
+        <label for="sb">Subject:</label><br>
+        <select name="subject_line" id="sb" required>
+            <option value="">--Select Subject--</option>
+            <option value="Email Support">Email Support</option>
+            <option value="eLearning Support">eLearning Support</option>
+            <option value="AMS Support">AMS Support</option>
+        </select><br><br>
+
+        <label for="sb">Message:</label><br>
+        <textarea name="client_message" id="" cols="30" rows="5" required></textarea><br><br>
+       
+        <input type="submit" name="send_message" value="Send Message">
+    </form>
+                </div>
        
         
         <div class="Instagram-link">
-            <p>Ask your questions on our <a href="https://www.instagram.com/kivu_kicks?igsh=MTFiZDNjMXVpMXE4bw%3D%3D&utm_source=qr" target="_blank">Instagram</a></p>
+            <p> our <a href="https://www.instagram.com/kivu_kicks?igsh=MTFiZDNjMXVpMXE4bw%3D%3D&utm_source=qr" target="_blank">Instagram</a></p>
         </div>
     </div>
     
--- Templates/index.php
+++ Templates/index.php
@@ -388,22 +388,49 @@ img{
                                                       font-style: italic; 
                                                       font-weight: 600;"> </h1>
                                                    
+                                                   <div class="row">
+    <div class="content">
+    <h1>Talk to Us</h1>
+    <form action="<?php print htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="contacts_form">
+        <label for="fn">Item name:</label><br>
+        <input type="text" id="fn" placeholder="Fullname" name="fullname" required><br><br>
 
-                                                    <form action="" methods="post">
-                                                    <label for="fn"> Size</label><br>
-                                                    <input type="text" id="fn">
+       
+
+        <label for="sb">Size:</label><br>
+        <select name="subject_line" id="sb" required>
+            <option value="">--Select size--</option>
+            <option value="Smal">S</option>
+            <option value="Medium">M</option>
+            <option value="shoes">L</option>
+            <option value="shoes">38</option>
+            <option value="shoes">39</option>
+            <option value="shoes">40</option>
+            <option value="shoes">41</option>
+            <option value="Large">42</option>
+        </select><br><br>
+
+        <label for="sb">Message:</label><br>
+        <textarea name="client_message" id="" cols="30" rows="5" required></textarea><br><br>
+       
+        <input type="submit" name="send_message" value="Send Message">
+    </form>
+                </div>
 <br>
                                                    
-                                                  
+<label for="sb">Delivery:</label><br>
+        <select name="subject_line" id="sb" required>
+            <option value="">--Delivery Mode--</option>
+            <option value="Delivery">Delivery</option>
+            <option value="Own pick up">Own pick up</option>
+           
+        </select><br><br>                                               
 
                
     
                 
 
-                        <ol >
-                            <input type="checkbox"  Pilot=" Pilot " value="on" /> Delivery
-                            <input type="checkbox"  Pilot=" Pilot " value="on" /> Own pick up
-                          
+                       
                             
                         </ol><form action=""></form>
                     <form>
