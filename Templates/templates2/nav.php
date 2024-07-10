0143b8bfdead4311cbb3beac4e4c4035c35d6287
Arsene ndimanya <146384225+Arsenendimanya151311@users.noreply.github.com>
2024-07-10 10:03:33 +0300

    loops

--- Templates/templates2/nav.php
+++ Templates/templates2/nav.php
@@ -31,14 +31,15 @@
     <div class="navdiv">
         <div class="logo"><a href="#"></a><h2>KIVU SNEAKERS</h2></div>
         <ul>
-            <li> <a href="About.php"> About Us </a> </li>
-            <li>  <a href="index.php"> Home </a>  </li>
-            <li> <a href="KivuCatalog.php">KivuCatalog</a> </li>
-            <li>  <a href="KidsCatolog.php">kidsCatalog</a></li>
-            <li> <a href="WomanKicksCatalog.php">WomanKicksCatalog</a> </li>
-            <li> <a href="Apparel.php">Apparel</a> </li>
-            <li>  <a href="Contact.php">Contact</a> </li>
-            <li>  <a href="signup.php">signup</a> </li>
+        <li> <a href="About.php"> About Us </a> </li>
+                <li>  <a href="index.php"> Home </a>  </li>
+                <li> <a href="KivuCatalog.php">KivuCatalog</a> </li>
+                <li>  <a href="KidsCatolog.php">kidsCatalog</a></li>
+                <li> <a href="WomanKicksCatalog.php">WomanKicksCatalog</a> </li>
+                <li> <a href="Apparel.php">Apparel</a> </li>
+                <li>  <a href="Contact.php">Contact</a> </li>
+                <li>  <a href="view_message.php">view_message</a> </li>
+                <li>  <a href="signup.php">signup</a> </li>
 
         </ul>
     
--- Templates/view_message.php
+++ Templates/view_message.php
@@ -1,25 +1,6 @@
-<nav class="navbar">
-    <div class="navdiv">
-        <div class="logo"><a href="#"></a>
-            <h2>KIVU SNEAKERS</h2>
-        </div>
-        <ul>
-            <li> <a href="About.php"> About Us </a> </li>
-            <li> <a href="index.php"> Home </a> </li>
-            <li> <a href="KivuCatalog.php">KivuCatalog</a> </li>
-            <li> <a href="KidsCatolog.php">kidsCatalog</a></li>
-            <li> <a href="WomanKicksCatalog.php">WomanKicksCatalog</a> </li>
-            <li> <a href="Apparel.php">Apparel</a> </li>
-            <li> <a href="Contact.php">Contact</a> </li>
-            <li> <a href="view_message.php">view_message</a> </li>
-            <li>  <a href="signup.php">signup</a> </li>
+<?php include_once ("templates2/nav.php")?>
 
-            
-        </ul>
 
-    </div>
-
-</nav>
 
 
 <?php
