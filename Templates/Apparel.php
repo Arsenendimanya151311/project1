81c17e4a40f92dd6c6130e96c5e4fce42eba2d90
Arsene ndimanya <146384225+Arsenendimanya151311@users.noreply.github.com>
2024-07-11 03:22:48 +0300

    apparel catalog updated  with footer

--- Templates/Apparel.php
+++ Templates/Apparel.php
@@ -159,9 +159,8 @@
                 
 
 
-                    <button>Submit</button>
-                    <hr size="8" width="100%" color="red"> 
-                    <p><center>Copyright KIVU Kicks 2024 &copy; Allrights reserved</center> </p>
+                   
+                                                           <?php include_once ("templates4/footer.php")?>
 
                     </form>
                     <style> body {
